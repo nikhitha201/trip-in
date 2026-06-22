e03513fecaadfb7033ee1433c892c12fda544e0d
Nikhitha <nikhithaponaganti14@gmail.com>
2026-06-22 18:15:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -708,3 +708,13 @@ ${place.hotel}
 });
 
 }
+const darkModeBtn =
+document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click",()=>{
+
+document.body.classList.toggle(
+"dark-mode"
+);
+
+});
